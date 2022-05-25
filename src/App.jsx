e1c2e97821dde86ed3608871e832bad7fbdcdf92
Luis Mauricio Carabajal Pires <luis.pires@softdesign.com.br>
2022-05-24 22:01:33 -0300

    adicionando a 3 api com os emails

--- src/App.jsx
+++ src/App.jsx
@@ -16,14 +16,16 @@ class App extends Component{
   loadPosts = async () =>{
     const postsResponse = fetch('https://jsonplaceholder.typicode.com/posts')
     const photosResponse = fetch('https://jsonplaceholder.typicode.com/photos')
+    const comentarisoResponse = fetch('https://jsonplaceholder.typicode.com/comments')
 
-    const [posts, photos] = await Promise.all([postsResponse, photosResponse]);
+    const [posts, photos, emails] = await Promise.all([postsResponse, photosResponse,comentarisoResponse]);
     
     const postsJson = await posts.json();
     const photosJson = await photos.json();
+    const emailsJson = await emails.json();
     
     const postsAndPhotos = postsJson.map((post, index) =>{
-      return { ...post,  cover: photosJson[index].url }
+      return { ...post,  cover: photosJson[index].url, comentario:emailsJson[index].email }
     })
     
    this.setState({ posts: postsAndPhotos})
@@ -38,8 +40,9 @@ class App extends Component{
     <div className="post">
       <img src={post.cover} alt={post.title}/>
       <div key={post.id} className="post-content">
-      <h1>{post.title}</h1>
-      <h4>{post.body}</h4>
+      <h1 className='titulo'>{post.title}</h1> 
+       { <h4>{post.body}</h4> }
+      <h3 className='email'>{post.comentario}</h3>
     </div>
     </div>
       ))}
